751501a5f203c752dd59bf8e0fed7fe5a1bb4696
Bruno Pavelja <bruno@pavelja.me>
2026-01-16 15:03:57 +0100

    Release 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.brunoafk.calendardnd"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10400
-        versionName = "1.4"
+        versionCode = 10500
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
